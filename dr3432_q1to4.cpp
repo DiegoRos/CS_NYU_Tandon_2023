b7e65a2e61330ed2cf6240e81b24fbe8d43f3dc6
Diego <diegorosenberg@gmail.com>
2023-03-02 16:36:44 -0600

    Added empty files for exam 2.

---
+++ dr3432_q1to4.cpp
@@ -0,0 +1,19 @@
+/*
+    TEST 2 QUESTIONS 1 TO 4
+    DIEGO ROSENBERG DE ANGOITIA
+    NetID: dr3432
+    Email: dr3432@nyu.edu
+*/
+
+/*
+1. 
+
+2. 
+
+
+3. 
+
+
+4. 
+
+*/
---
+++ dr3432_q6.cpp
@@ -0,0 +1,19 @@
+
+/*
+    TEST 2 QUESTION 6
+    DIEGO ROSENBERG DE ANGOITIA
+    NetID: dr3432
+    Email: dr3432@nyu.edu
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+
+
+}
